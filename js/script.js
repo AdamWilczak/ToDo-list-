7eb8334031bda4f6186da0274eaaa5754abf855e
Adam Wilczak <adam.wilczak11@gmail.com>
2023-03-18 22:05:34 +0100

    adding splice(r)

--- js/script.js
+++ js/script.js
@@ -8,19 +8,38 @@
       content: "wyczesać niedżwiedzia",
       done: true,
     },
+    
   ];
+
+ 
 
   const render = () => {
     let htmlString = "";
     for (const task of tasks) {
-      htmlString += `
+      htmlString +=`
       <li
       ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-      ${tasks.content}
+      <button class="js-remove">usun</button>
+
+      ${tasks.content};
       </li>
       `;
-    }
+    };
+    
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        tasks.splice(index, 1);
+        render();
+      })
+    })
+  
+
+
+
   };
 
   const addNewTask = (newTaskContent) => {
