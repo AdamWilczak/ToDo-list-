0a994b69ef33937298a333de9cec589132166201
Adam Wilczak <adam.wilczak11@gmail.com>
2023-03-19 12:13:36 +0100

    formatung code

--- js/script.js
+++ js/script.js
@@ -8,56 +8,73 @@
       content: "wyczesać niedżwiedzia",
       done: true,
     },
-    
   ];
-
- 
-
-  const render = () => {
-    let htmlString = "";
-    for (const task of tasks) {
-      htmlString +=`
-      <li
-      ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-      <button class="js-remove">usun</button>
-
-      ${tasks.content};
-      </li>
-      `;
-    };
-    
-    document.querySelector(".js-tasks").innerHTML = htmlString;
-
-    const removeButtons = document.querySelectorAll(".js-remove");
-
-    removeButtons.forEach((removeButton, index) => {
-      removeButton.addEventListener("click", () => {
-        tasks.splice(index, 1);
-        render();
-      })
-    })
-  
-
-
-
-  };
 
   const addNewTask = (newTaskContent) => {
     tasks.push({
       content: newTaskContent,
     });
+
     render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, taskIndex) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(taskIndex);
+      });
+    });
+
+    const doneButtons = document.querySelectorAll(".js-done");
+
+    doneButtons.forEach((doneButton, taskIndex) => {
+      doneButton.addEventListener("click", () => {
+        toggleTaskDone(taskIndex);
+      });
+    });
+  };
+
+  const render = () => {
+    let htmlString = "";
+    for (const task of tasks) {
+      htmlString += `
+            <li class="task">
+            <button class="js-done task__button task__button--done">${task.done ? "✔" : ""}</button>
+            <span class="${task.done ? "task--done" : ""}">${task.content}</span>
+            <button class="js-remove task__button task__button--remove">🗑</button>
+            </li>
+            `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
   };
 
   const onFormSubmit = (event) => {
     event.preventDefault();
 
-    const newTaskContent = document.querySelector(".js-newTask").value.trim();
-    if (newTaskContent === "") {
-      return;
+    const newTaskItem = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskItem.value.trim();
+
+    if (newTaskContent !== "") {
+      addNewTask(newTaskContent);
     }
 
-    addNewTask(newTaskContent);
+    newTaskItem.focus();
+    newTaskItem.value = "";
   };
 
   const init = () => {
@@ -66,9 +83,7 @@
     const form = document.querySelector(".js-form");
 
     form.addEventListener("submit", onFormSubmit);
-  
   };
 
   init();
-
-};
+}
